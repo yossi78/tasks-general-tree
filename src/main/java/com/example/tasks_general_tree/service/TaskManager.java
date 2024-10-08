0a7c58bc02f4a8cd0bf06e301115620f0a72cf00
yossi78 <yossi78@gmail.com>
2024-10-08 20:00:18 +0300

    fix bl

--- src/main/java/com/example/tasks_general_tree/service/TaskManager.java
+++ src/main/java/com/example/tasks_general_tree/service/TaskManager.java
@@ -35,7 +35,7 @@ public class TaskManager {
 
 
     public void addSubTask(String parentName,String name) throws IllegalAccessException {
-        if(taskMap.containsKey(parentName) && !taskMap.containsKey(name) ){
+        if(!taskMap.containsKey(parentName) || taskMap.containsKey(name) ){
             log.warn("Parent not exists or the task with name of :"+name + " is already exists");
             throw  new IllegalAccessException();
         }
@@ -66,50 +66,57 @@ public class TaskManager {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         TaskManager taskManager = new TaskManager();
         // Create the root node
 
+        taskManager.addTask("Child 1");
+        taskManager.addTask("Child 2");
+        taskManager.addTask("Child 3");
+
+//        Task rootTask = new Task("root",LocalDateTime.now());
+//        TreeNode<Task> root = new TreeNode<>(rootTask);
+//
+//
+//        // Create child tasks
+//        Task childTask1 = new Task("Child 1",LocalDateTime.now());
+//        Task childTask2 = new Task("Child 2",LocalDateTime.now());
+//        Task childTask3 = new Task("Child 3",LocalDateTime.now());
+//        // Create child nodes
+//        TreeNode<Task> child1 = new TreeNode<>(childTask1);
+//        TreeNode<Task> child2 = new TreeNode<>(childTask2);
+//        TreeNode<Task> child3 = new TreeNode<>(childTask3);
+//
+//
+//        // Add children to root
+//        taskManager.addTask("child1");
+//        taskManager.addTask("child2");
+//        taskManager.addTask("child3");
+//
+//
+//        // Create grandchildren tasks
+//        Task grandChildTask1 = new Task("GrandChild 1",LocalDateTime.now());
+//        Task grandChildTask2 = new Task("GrandChild 2",LocalDateTime.now());
+//        Task grandChildTask3 = new Task("GrandChild 3",LocalDateTime.now());
 
 
-        Task rootTask = new Task("root",LocalDateTime.now());
-        TreeNode<Task> root = new TreeNode<>(rootTask);
+        taskManager.addSubTask("Child 1","GrandChild 1");
+        taskManager.addSubTask("Child 1","GrandChild 2");
+        taskManager.addSubTask("Child 1","GrandChild 3");
 
-
-        // Create child tasks
-        Task childTask1 = new Task("Child 1",LocalDateTime.now());
-        Task childTask2 = new Task("Child 2",LocalDateTime.now());
-        Task childTask3 = new Task("Child 3",LocalDateTime.now());
-        // Create child nodes
-        TreeNode<Task> child1 = new TreeNode<>(childTask1);
-        TreeNode<Task> child2 = new TreeNode<>(childTask2);
-        TreeNode<Task> child3 = new TreeNode<>(childTask3);
-
-
-        // Add children to root
-        root.addChild(child1);
-        root.addChild(child2);
-        root.addChild(child3);
-
-
-        // Create grandchildren tasks
-        Task grandChildTask1 = new Task("GrandChild 1",LocalDateTime.now());
-        Task grandChildTask2 = new Task("GrandChild 2",LocalDateTime.now());
-        Task grandChildTask3 = new Task("GrandChild 3",LocalDateTime.now());
-
-
-        // Add grandchildren
-        TreeNode<Task> grandChild1 = new TreeNode<>(grandChildTask1);
-        TreeNode<Task> grandChild2 = new TreeNode<>(grandChildTask2);
-        TreeNode<Task> grandChild3 = new TreeNode<>(grandChildTask3);
-
-
-        child1.addChild(grandChild1);
-        child1.addChild(grandChild2);
-        child1.addChild(grandChild3);
+//
+//        // Add grandchildren
+//        TreeNode<Task> grandChild1 = new TreeNode<>(grandChildTask1);
+//        TreeNode<Task> grandChild2 = new TreeNode<>(grandChildTask2);
+//        TreeNode<Task> grandChild3 = new TreeNode<>(grandChildTask3);
+//
+//
+//        child1.addChild(grandChild1);
+//        child1.addChild(grandChild2);
+//        child1.addChild(grandChild3);
 
         // Display the tree
-        GeneralTreeUtil.printTree(root, " ");
+        GeneralTreeUtil.printTree(taskManager.root, " ");
     }
 
 
