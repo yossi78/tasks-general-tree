24793349dfc337428890458987f10838dcb6d857
yossi78 <yossi78@gmail.com>
2024-10-11 15:26:32 +0300

    remove remarks

--- src/main/java/com/example/tasks_general_tree/service/TaskManager.java
+++ src/main/java/com/example/tasks_general_tree/service/TaskManager.java
@@ -4,10 +4,8 @@ import com.example.tasks_general_tree.model.Task;
 import com.example.tasks_general_tree.model.TreeNode;
 import com.example.tasks_general_tree.util.GeneralTreeUtil;
 import lombok.extern.slf4j.Slf4j;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
